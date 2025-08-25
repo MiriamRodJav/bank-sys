881fa2bac6b36040cb67093800319696ebe807f0
Cindiruela <Mendozaic@hotmail.com>
2025-08-25 16:29:07 -0500

    repository functions

--- src/main/java/com/nttdata/bank/repository/ClientRepository.java
+++ src/main/java/com/nttdata/bank/repository/ClientRepository.java
@@ -1,2 +1,13 @@
-package com.nttdata.bank.repository;public interface ClientRepository {
+package com.nttdata.bank.repository;
+
+import com.nttdata.bank.model.ClientModel;
+
+import java.util.Optional;
+
+public interface ClientRepository {
+    ClientModel save(ClientModel client);
+    Optional<ClientModel> findByDni(String dni);
+
+    boolean delete(String dni);
+    boolean existsByDni(String dni);
 }
